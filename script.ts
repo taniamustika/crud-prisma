f8666f651315c416eabf47915ab610538a662fa2
taniamustika <taniasugiharto@gmail.com>
2023-11-28 22:20:09 +0700

    Read - Get all records

--- script.ts
+++ script.ts
@@ -3,21 +3,8 @@ import { Prisma, PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  //By unique identifier
-  // const user = await prisma.user.findUnique({
-  //   where: {
-  //     email: 'elsa@prisma.io',
-  //   },
-  // })
-  // console.log(user)
-
-  //By ID
-  const user = await prisma.user.findUnique({
-    where: {
-      id: 5,
-    },
-  })
-  console.log(user)
+  const users = await prisma.user.findMany()
+  console.log(users)
 }
 
 main()
