599ae54cd1280a45646dcdfd0fa67ad99d3a8928
taniamustika <taniasugiharto@gmail.com>
2023-11-29 14:37:25 +0700

    Read - Filter by multiple field values - Add comment & Logging features

--- script.ts
+++ script.ts
@@ -1,6 +1,31 @@
 import { Prisma, PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'stdout',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+})
+
+prisma.$on('query' as never, (e: any) => {
+  console.log('Query: ' + e.query)
+  console.log('Params: ' + e.params)
+  console.log('Duration: ' + e.duration + 'ms')
+})
 
 async function main() {
   // (name === StartWith('E') OR (Profile Views > 0 AND Role === 'ADMIN'))
